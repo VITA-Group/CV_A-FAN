83a14fb42d76c9a3a6928cd09016fce8afd08867
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-30 17:32:43 +0800

    Delete Segmentation directory

--- Segmentation/sh/voc/2012/clean50/00_baseline50.sh
+++
@@ -1,9 +0,0 @@
-GPU=${1}
-python -u main_ori.py \
---year 2012 \
---crop_val \
---model deeplabv3plus_resnet50 \
---gpu_id ${GPU} \
---batch_size 4 \
---random_seed 66 \
-baseline_voc2012_resnet50_bs4_seed66
